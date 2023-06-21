d1ac787ae7f739690398ab89a6dd1965772e377e
LeoPhoneEgypt <liangyujui@gmail.com>
2023-06-21 11:12:51 +0800

    0620update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,29 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     private int score;
-    [SerializeField] private TMP_Text scoreboardUI;
+    private string score_text = "Score:";
+    [SerializeField]
+    private TMP_Text scoreboard_UI;
 
     public bool isGameover;
-    private string CurrentScoreText = "Score: ", FinalScoreText = "Final Score: ";
-    [SerializeField] private GameObject gameoverPage;
-    [SerializeField] private TMP_Text finalScore;
-    [SerializeField] private Button restartButton;
+    private string final_score_text = "Final Score: ";
+    [SerializeField]
+    private GameObject Gameover_Page;
+    [SerializeField]
+    private TMP_Text final_score;
+    [SerializeField]
+    private Button Restart_button;
+
+    [SerializeField]
+    private PlatformsMovement LeftMovement;
+    [SerializeField]
+    private PlatformsMovement RightMovement;
+    [SerializeField]
+    private PlatformsMovement MiddleMovement;
+
+    [SerializeField]
+    private ObjectFalling objectFalling;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,27 +39,43 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        scoreboardUI.text = CurrentScoreText + score; // Update current score
+        scoreboard_UI.text = score_text + score;
+        //add_point(5);
+
     }
 
     private void start_game()
     {
         isGameover = false;
-        gameoverPage.SetActive(false);
+        Gameover_Page.SetActive(false);
         score = 0;
+        objectFalling.enabled = true;
+        LeftMovement.enabled = true;
+        MiddleMovement.enabled = true;
+        RightMovement.enabled = true;
     }
 
     public void add_point(int points)
     {
-        if (!isGameover) score += points; // If not gameover, update the score.
+        if (!isGameover)
+        {
+            score += points;
+        }
     }
 
     public void game_over()
     {
         Debug.Log("Game Over");
         isGameover = true;
-        finalScore.text = FinalScoreText + score;
-        gameoverPage.SetActive(true);
-        restartButton.onClick.AddListener(start_game);
+        final_score.text = final_score_text + score;
+        Gameover_Page.SetActive(true);
+        Restart_button.onClick.AddListener(start_game);
+        objectFalling.enabled = false;
+        LeftMovement.enabled = false;
+        MiddleMovement.enabled = false;
+        RightMovement.enabled = false;
     }
+
+    // Update is called once per frame
+    
 }
